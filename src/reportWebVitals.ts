72bf9af7936809ad5dd0fed3d50a7fd97db806a8
Armen Kesablyan <armen@twoowl.com>
2023-02-05 16:53:27 -0800

    Added linting support

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,19 @@
-import { ReportHandler } from 'web-vitals';
+import {
+  type ReportHandler,
+  getCLS,
+  getFCP,
+  getFID,
+  getLCP,
+  getTTFB,
+} from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+  if (onPerfEntry != null && onPerfEntry instanceof Function) {
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
