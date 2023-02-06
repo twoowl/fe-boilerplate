42d376f8e4493c277ddc747775024cd317fc7e19
Armen Kesablyan <armen@twoowl.com>
2023-02-06 15:16:17 -0800

    Modified config to expose boolean values

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@
  * runtime and should not change once set.
  *
  * Configurations can be set by:
- *  - Querystrings: Use lowercase prefixed with debug-${configuration} to match.
+ *  - Querystrings: Use lowercase prefixed with app-${configuration} to match.
  *  - Environment: Use uppercase prefixed with REACT_APP_${configuration} to match.
  * _Note: Querystrings will overwrite environment configurations.
  */
@@ -12,14 +12,16 @@ import pkg from '../package.json';
 
 const env = process.env;
 const envPrefix = 'REACT_APP';
-const queryPrefix = 'debug';
+const queryPrefix = 'app';
 
 const params = new URLSearchParams(window.location.search.toLowerCase());
 
 const config = {
   environment: (env.NODE_ENV || 'development'),
+  debugMode: Boolean((params.get(`${queryPrefix}-debug`) ?? env[`${envPrefix}_DEBUG`]) ?? 'false'),
   version: (pkg.version || '0.0.0'),
-  publicUrl: (params.get(`${queryPrefix}-public-url`) ?? env[`${envPrefix}_PUBLIC_URL`]) ?? '/'
+  publicUrl: (params.get(`${queryPrefix}-public-url`) ?? env[`${envPrefix}_PUBLIC_URL`]) ?? '/',
+  enableAnalytics: Boolean((params.get(`${queryPrefix}-analytics`) ?? env[`${envPrefix}_ANALYTICS`]) ?? 'true'),
 }
 
 export default config;
