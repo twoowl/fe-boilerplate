9ab0951073a9807a66ff937ec6ad0df18b31d26a
Armen Kesablyan <armen@twoowl.com>
2023-02-06 16:43:29 -0800

    Removed WebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 
-import reportWebVitals from './reportWebVitals';
 import Routing from './routing';
 
 const GlobalCSS = css`
@@ -28,8 +27,3 @@ root.render(
     <Global styles={GlobalCSS} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
